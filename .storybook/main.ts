cdfb8f28297bc80162c293ea96bd96379ab7844d
fahsai <fahsai.srip@gmail.com>
2024-04-18 17:57:24 +0700

    update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,41 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from '@storybook/nextjs';
+const path = require('path');
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@chakra-ui/storybook-addon",
-    "@storybook/addon-mdx-gfm"
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@chakra-ui/storybook-addon',
+    '@storybook/addon-mdx-gfm',
   ],
 
   framework: {
-    name: "@storybook/nextjs",
-    options: {}
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
   docs: {
-    autodocs: "tag"
+    autodocs: 'tag',
   },
-  staticDirs: ["../public"]
+  staticDirs: ['../public'],
+  webpackFinal: async (config: any) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+
+    return config;
+  },
 };
 export default config;
